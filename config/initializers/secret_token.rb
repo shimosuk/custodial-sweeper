559337f4e624c81cd0fce3c490a66f0c4e4e817e
ryosuke shimomura <r-shimomura@esm.co.jp>
2012-07-26 10:48:07 +0900

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CustodialSweeper::Application.config.secret_token = '2a068a31c8cd3c697f173422729b98da962f54a3c82b6fa3844001d04b79f4fedef3e3713db6c386cbaad09205d2fbb2d1f9102de1def640128c62ff19077ce2'
