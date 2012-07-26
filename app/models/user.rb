3541a52c0374051d14120d402db993dbba5c793c
ryosuke shimomura <r-shimomura@esm.co.jp>
2012-07-26 14:27:09 +0900

    historyにしたけどココをアプリの画面にすると決めた日
    とりあえずuser情報を持ちだした

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   attr_accessible :name, :roll, :user_id
   validates :name, presence: true
   validates :user_id, presence: true
+
+  scope :able, where(roll: true)
+  scope :unable, where(roll: false)
 end
