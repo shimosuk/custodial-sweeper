31fd0a5819b270d7a6ab610d35108d0e9aee09a0
ryosuke shimomura <r-shimomura@esm.co.jp>
2012-07-26 13:13:52 +0900

    rake db:migrate:up VERSION= で指定したテーブル作ったらできた

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120726021134) do
+ActiveRecord::Schema.define(:version => 20120726025159) do
+
+  create_table "items", :force => true do |t|
+    t.string   "item_id"
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "items", ["item_id"], :name => "index_items_on_item_id"
 
   create_table "users", :force => true do |t|
     t.string   "user_id"
